bfcfb90fe9fa776027fdecc3396e84b7b4a2486e
nishmsyr <danishamsyar06@gmail.com>
2025-05-19 03:18:41 +0800

    Weather App

--- script.js
+++ script.js
@@ -1,30 +1,142 @@
-const location = document.getElementById("location"),
-converter = document.getElementById("converter"),
-weatherIcon = document.getElementById(".weather-icon"),
-temperature = document.getElementById(".temperature"),
-feelsLike = document.getElementById(".feelsLike"),
-date = document.getElementById(".date"),
-city = document.getElementById(".city"),
-HValue = document.getElementById("HValue"),
-WValue = document.getElementById("WValue"),
-SRValue = document.getElementById("SRValue"),
-SSValue = document.getElementById("SSValue"),
-CValue = document.getElementById("CValue"),
-UVValue = document.getElementById("UVValue"),
-PValue = document.getElementById("PValue"),
-forecast = document.querySelector(".forecast");
+const cityInput = document.querySelector(".city-input");
+const searchBtn = document.querySelector(".search-btn");
+const notFoundSection = document.querySelector(".not-found");
+const searchCitySection = document.querySelector(".search-city");
+const weatherInfoSection = document.querySelector(".weather-info");
+const forecastItemContainer = document.querySelector(
+  ".forecast-item-container"
+);
 
-WEATHER_API_ENDPOINT='https://api.openweathermap.org/data/2.5/weather?appid=a5bb4718b30b6f5858697997567fffa&q=';
-WEATHER_DATA_ENDPOINT='https://api.openweathermap.org/data/2.5/onecall?appid=a5bb4718b30b6f5858697997567fffa&exclude=minutely&units=metric&'
+const countryTxt = document.querySelector(".country-txt");
+const temptTxt = document.querySelector(".temp-txt");
+const conditionTxt = document.querySelector(".condition-txt");
+const humidityValueTxt = document.querySelector(".humidity-value-txt");
+const windValueTxt = document.querySelector(".wind-value-txt");
+const weatherSummaryImg = document.querySelector(".weather-summary-img");
+const currentDateTxt = document.querySelector(".current-date-txt");
 
-function findLocation() {
-    fetch(WEATHER_API_ENDPOINT+location.value).then((response)=>response.json()).then((data) => {
-        if(data.cod!='' && data.cod!=200){
-            alert(data.message);
-            return;
-        }
-        console.log(data.coord.lon, data.coord.lat); });
-    })
+const apiKey = "cdd131fae455315375ea6d8a5183ecdc";
+
+searchBtn.addEventListener("click", () => {
+  if (cityInput.value.trim() != "") {
+    updateWeatherInfo(cityInput.value);
+    cityInput.value = "";
+    cityInput.blur();
+  }
+});
+
+cityInput.addEventListener("keydown", (event) => {
+  if (event.key == "Enter" && cityInput.value.trim() != "") {
+    updateWeatherInfo(cityInput.value);
+    cityInput.value = "";
+    cityInput.blur();
+  }
+});
+
+async function getFetchData(endPoint, city) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}&units=metric`;
+
+  const response = await fetch(apiUrl);
+  return response.json();
 }
 
+function getWeatherIcon(id) {
+  if (id <= 232) return "thunderstorm.svg";
+  if (id <= 321) return "drizzle.svg";
+  if (id <= 531) return "rain.svg";
+  if (id <= 622) return "snow.svg";
+  if (id <= 781) return "atmosphere.svg";
+  if (id <= 800) return "clear.svg";
+  else return "clouds.svg";
+}
 
+function getCurrentDate() {
+  const currentDate = new Date();
+  const options = {
+    weekday: "short",
+    day: "2-digit",
+    month: "short",
+  };
+  return currentDate.toLocaleDateString("en-GB", options);
+}
+
+async function updateWeatherInfo(city) {
+  const weatherData = await getFetchData("weather", city);
+
+  if (weatherData.cod != 200) {
+    showDisplaySection(notFoundSection);
+    return;
+  }
+
+  const {
+    name: country,
+    main: { temp, humidity },
+    weather: [{ id, main }],
+    wind: { speed },
+  } = weatherData;
+
+  countryTxt.textContent = country;
+  temptTxt.textContent = Math.round(temp) + " °C";
+  conditionTxt.textContent = main;
+  humidityValueTxt.textContent = humidity + "%";
+  windValueTxt.textContent = speed + " M/s";
+
+  currentDateTxt.textContent = getCurrentDate();
+  weatherSummaryImg.scr = `./assets/weather/${getWeatherIcon(id)}`;
+
+  await updateForecastInfo(city);
+  showDisplaySection(weatherInfoSection);
+}
+
+async function updateForecastInfo(city) {
+  const forecastData = await getFetchData("forecast", city);
+  const timeTaken = "12:00:00";
+  const todayDate = new Date().toISOString().split("T")[0];
+
+  forecastItemContainer.innerHTML = "";
+  forecastData.list.forEach((forecastWeather) => {
+    if (
+      forecastWeather.dt_txt.includes(timeTaken) &&
+      !forecastWeather.dt_txt.includes(todayDate)
+    ) {
+      updateForecastItems(forecastWeather);
+    }
+  });
+}
+
+function updateForecastItems(weatherData) {
+  const {
+    dt_txt: date,
+    weather: [{ id }],
+    main: { temp },
+  } = weatherData;
+
+  const dateTaken = new Date(date);
+  const dateOption = {
+    day: "2-digit",
+    month: "short",
+  };
+  const dateResult = dateTaken.toLocaleDateString("en-US", dateOption);
+
+  const forecastItem = `
+            <div class="forecast-item">
+                <h5 class="forecast-item-date regular-txt">${dateResult}</h5>
+                <img
+                src="./assets/weather/${getWeatherIcon(id)}"
+                alt="Weather icon"
+                class="forecast-item-img"
+                />
+                <h5 class="forecast-item-temp">${Math.round(temp)}</h5>
+            </div>
+    `;
+
+  forecastItemContainer.insertAdjacentHTML("beforeend", forecastItem);
+}
+
+function showDisplaySection(section) {
+  [weatherInfoSection, searchCitySection, notFoundSection].forEach(
+    (section) => (section.style.display = "none")
+  );
+
+  section.style.display = "flex";
+}
